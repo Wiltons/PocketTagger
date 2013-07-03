40f6da8d1b78fb399babba5caf337e30be9b903f
Wiltons <cillians@gmail.com>
2013-07-03 23:23:48 +0100

    Initial working copy to tag long and/or short articles based on the length (wordcount) of an article

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-test
+A Python script to tag articles in Pocket (http://getpocket.com) as 'short' or 'long' based on the wordcount of the article.
+
+Thanks to tbunnyman for work on Pocket-Calibre plugin which I used as reference (https://github.com/tbunnyman/ReadItLater-Calibre-Plugin/)
+
+List splitter unceremoniously plucked from StackOverflow (http://stackoverflow.com/a/752562)
+
+
+Version 0.1
+   * Hardcoded tags (short, very_long)
+   * Hardcoded parameters for the length of a short or long article
+   * Optional to only tag long or short articles
+   * Splits modification URL to chunks if URL is too large (hardcoded at 50 items)
+
+TODO:
+   * Fix hardcoded variables
+   * Smart tag based on length of articles previously tagged as 'X'
+   * Fetch_item functions don't need so much information from the item
+   * Proper error handling
+   * OAuth?
+
--- pocketTagger.py
+++ pocketTagger.py
@@ -7,14 +7,20 @@ import urllib2
 from time import strftime
 
 apikey = 'cH6gdvT0p0xK4Vc61aT7779o2cAqN8aM'
-index_url = u'http://getpocket.com'
+index_url = u'https://getpocket.com'
 read_api_url = index_url + u'/v3/get'
 modify_api_url = index_url + u'/v3/send'
 only_pull_tag = None
 articles = []
+# Fill in username and password
 username = ''
 password = ''
+# Wordcount parameters for tagging
 shortSize = 2000
+longSize = 10000
+# Set to true/false to only tag long or short
+tagLong = True
+tagShort = True
 
 def get_auth_uri():
 	#Quick function to return the authentication part of the url
@@ -35,15 +41,15 @@ def get_pull_articles_uri():
 		uri = u'{0}&tag={1}'.format(uri, only_pull_tag)
 	return uri
 	
-def parse_index():
+def fetch_items_short():
 	pocket_feed = []
 	fetch_url = u"{0}?{1}{2}".format(
 		read_api_url,
 		get_auth_uri(),
 		get_pull_articles_uri()
 	)
-	print(fetch_url)
-	articles = []
+	shortItems = []
+	#print(fetch_url)
 	try:
 		request = urllib2.Request(fetch_url)
 		response = urllib2.urlopen(request)
@@ -56,14 +62,10 @@ def parse_index():
 		#self.log.exception("Unable to connect to getpocket.com's api: {0}\nurl: {1}".format(e, fetch_url))
 		return []
 
-#	if len(pocket_feed) < self.minimum_articles:
-#		self.mark_as_read_after_dl = False
-#		self.user_error("Only {0} articles retrieved, minimum_articles not reached".format(len(pocket_feed)))
-
 	for pocket_article in pocket_feed.iteritems():
-		print ('Item id is {0} and is length {1}'.format(pocket_article[1]['item_id'],pocket_article[1]['word_count']))
-		if int(pocket_article[1]['word_count']) < shortSize:
-			articles.append({
+		#print ('Item id is {0} and is length {1}'.format(pocket_article[1]['item_id'],pocket_article[1]['word_count']))
+		if (int(pocket_article[1]['word_count']) <= shortSize):
+			shortItems.append({
 				'item_id': pocket_article[0],
 				'title': pocket_article[1]['resolved_title'],
 				'date': pocket_article[1]['time_updated'],
@@ -73,31 +75,99 @@ def parse_index():
 				'sort': pocket_article[1]['sort_id'],
 				'size': pocket_article[1]['word_count']
 			})
-			print('Added item {0}'.format(pocket_article[1]['item_id']))
-	articles = sorted(articles, key=operator.itemgetter('sort'))
-	actions_list = []
-	tags = ['short']
-	for article_id in articles:
-		actions_list.append({
-			'action': 'tags_add',
-			'item_id': articles[0]['item_id'],
-			'tags': tags
-		})
-	mark_read_url = u'{0}?actions={1}{2}'.format(
-		modify_api_url,
-		json.dumps(actions_list, separators=(',', ':')),
-		get_auth_uri()
+			#print('Added item {0}'.format(pocket_article[1]['resolved_url']))
+		else:
+			pass
+				
+	shortItems = sorted(shortItems, key=operator.itemgetter('sort'))
+	return shortItems
+	
+def fetch_items_long():
+	pocket_feed = []
+	fetch_url = u"{0}?{1}{2}".format(
+		read_api_url,
+		get_auth_uri(),
+		get_pull_articles_uri()
 	)
-	print(mark_read_url)
+	longItems = []
+	#print(fetch_url)
 	try:
-		request = urllib2.Request(mark_read_url)
+		request = urllib2.Request(fetch_url)
 		response = urllib2.urlopen(request)
+		pocket_feed = json.load(response)['list']
 	except urllib2.HTTPError as e:
-		print('Pocket returned an error while archiving articles: {0}'.format(e))
+		#self.log.exception("Pocket returned an error: {0}".format(e.info()))
+		print('Pocket returned an error: {0}'.format(e.info()))
 		return []
 	except urllib2.URLError as e:
-		print("Unable to connect to getpocket.com's modify api: {0}".format(e))
+		#self.log.exception("Unable to connect to getpocket.com's api: {0}\nurl: {1}".format(e, fetch_url))
 		return []
-	return [articles]
+
+	for pocket_article in pocket_feed.iteritems():
+		#print ('Item id is {0} and is length {1}'.format(pocket_article[1]['item_id'],pocket_article[1]['word_count']))
+		if (int(pocket_article[1]['word_count']) >= longSize):
+			longItems.append({
+				'item_id': pocket_article[0],
+				'title': pocket_article[1]['resolved_title'],
+				'date': pocket_article[1]['time_updated'],
+				'url': u'{0}/a/read/{1}'.format(index_url, pocket_article[0]),
+				'real_url': pocket_article[1]['resolved_url'],
+				'description': pocket_article[1]['excerpt'],
+				'sort': pocket_article[1]['sort_id'],
+				'size': pocket_article[1]['word_count']
+			})
+			#print('Added item {0}'.format(pocket_article[1]['item_id']))
+		else:
+			pass
+				
+	longItems = sorted(longItems, key=operator.itemgetter('sort'))
+	return longItems
+	
+	
+def tagItems(mark_list, tags=None):
+	actions_list = []
+	split_array = []
+	for article_id in mark_list:
+		actions_list.append({
+			'action': 'tags_add',
+			'item_id': article_id,
+			'tags': tags
+		})
+		#print("Added {0}".format(article_id))
+	
+	#print(len(actions_list))
+	if len(actions_list) > 50:
+		chunks = (len(actions_list) / 50) + 1
+		#print(chunks)
+		split_array = split_list(actions_list, chunks)
 		
-parse_index()
+	for partition in split_array:
+		#print(len(partition))
+		tag_url = u'{0}?actions={1}{2}'.format(
+			modify_api_url,
+			json.dumps(partition, separators=(',', ':')),
+			get_auth_uri()
+		)
+		#print("Tag url: {0}".format(tag_url))
+		try:
+			request = urllib2.Request(tag_url)
+			response = urllib2.urlopen(request)
+		except urllib2.HTTPError as e:
+			print('Pocket returned an error while archiving articles: {0}'.format(e))
+			return []
+		except urllib2.URLError as e:
+			print("Unable to connect to getpocket.com's modify api: {0}".format(e))
+			return []
+	return []
+
+def split_list(alist, wanted_parts=1):
+    length = len(alist)
+    return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts] 
+             for i in range(wanted_parts) ]
+
+if tagLong:
+	longList = fetch_items_long()
+	tagItems([x['item_id'] for x in longList], ['very_long'])
+if tagShort:
+	shortList = fetch_items_short()
+	tagItems([x['item_id'] for x in shortList], ['short'])
